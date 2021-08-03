a84c1b433524767179a2b5f5f6005357fdc4f568
Marco Castellana <arkanoidmark96@gmail.com>
2021-08-03 20:23:33 +0100

    MC: Added create new todo function

--- Front-end/js/toDoList.js
+++ Front-end/js/toDoList.js
@@ -2,7 +2,7 @@
   let toDoContainer = document.querySelector(".to-do-container");
   let crudSelect = document.querySelector(".form-select");
   
-
+  //display form to submit to-do id
   const displayOneToDoForm = () => {
     const form = `
       <section class="text-center">
@@ -33,57 +33,83 @@
     })
   }
 
+  //display form to submit a new to-do
+  const displayAddForm = () => {
+    const form = `
+      <section class="text-center">
+        <p class="mb-4">Please insert the details of a <strong>new</strong> to do</p>
+        <form class="form-todo">
+          <div class="col-8 mx-2">
+            <input 
+              class="form-control" 
+              type="text"  
+              id="todo-title" 
+              placeholder="Title"
+            >
+          </div>
+          <div class="col-4">
+            <button class="btn btn-primary">Add new todo</button>
+          </div>
+        </form>
+      </section>
+      `;
+    toDoContainer.innerHTML = form;
+
+    let btn = document.querySelector(".btn");
+    
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      let value = document.querySelector("#todo-title").value;
+      toDoContainer.innerHTML = "";
+      addToDo(new ToDo(value, false));
+    })
+  }
+
+  //Displays all todos onto the DOM
   const displayAllToDosResult = (data) => {
     setStyleContainer();
+    let ul = document.createElement("ul");
+    ul.classList.add("todos");
+    toDoContainer.appendChild(ul);
     for (let i = 0; i < data.length; i++) {
       let el;
       if (data[i].completed) {
-        el = `
-          <input
-          type="checkbox"
-          name=${data[i].todosId } 
-          id=${ data[i].todosId } 
-          value=${ data[i].todosId }
-          checked
-        >
-        <label for="todo1"><del>${data[i].title}</del></label><br>
-        `;
+        el = `<li><del>${ data[i].title }</del></li>`;
       } else {
-        el = `
-        <input
-          type="checkbox"
-          name=${data[i].todosId } 
-          id=${ data[i].todosId } 
-          value=${ data[i].todosId }
-        >
-        <label for="todo1">${data[i].title}</label><br>
-        `;
+        el = `<li>${ data[i].title }</li>`;
       }
-      toDoContainer.innerHTML += el;
+      ul.innerHTML += el;
     }
-
   }
 
+  //Displays one todo onto the DOM
   const displayOneToDoResult = (data) => {
     setStyleContainer();
+    let ul = document.createElement("ul");
+    ul.classList.add("todos");
+    toDoContainer.appendChild(ul);
     let el;
     if (data.completed) {
-       el = ` <label for="todo1"><del>${ data.title }</del></label><br>`;
+      el = `<li><del>${ data.title }</del></li>`;
     } else {
-      el = ` <label for="todo1">${ data.title }</label><br>`;
+      el = `<li>${ data.title }</li>`;
     }
     toDoContainer.innerHTML = el;
   }
 
+  //Displays background style
   const setStyleContainer = () => {
     toDoContainer.classList.remove("empty-container");
   }
 
+  //Clear background style and remove any element previosly added
   const clearToDoContainer = () => {
     toDoContainer.innerHTML = "";
     toDoContainer.classList.add("empty-container");
   }
 
+  //Retrieve the value chosen from dropdown and do a CRUD operation
+  //accordingly
   const getOptionValue = () => {
     clearToDoContainer();
     let value = crudSelect.options[crudSelect.selectedIndex].value;
@@ -95,6 +121,7 @@
         displayOneToDoForm();
         break;
       case "create":
+        displayAddForm();
         break;
       case "update":
         break;
@@ -105,6 +132,7 @@
     }
   }
   
+  //GET all fetch API
   const getAllToDos = () => {
     fetch("http://localhost:8080/todos")
     .then(response => response.json())
@@ -112,6 +140,7 @@
     .catch(error => console.error(error));
   }
   
+  //GET one fetch API
   const getOneToDo = (id) => {
     fetch(`http://localhost:8080/todos/${id}`)
     .then(response => response.json())
@@ -119,6 +148,7 @@
     .catch(error => console.error(error));
   }
   
+  //POST fetch API
   const addToDo = (toDo) => {
     fetch("http://localhost:8080/todos", {
       method: 'POST',
@@ -132,6 +162,7 @@
     .catch(error => console.error(error));
   }
   
+  //PUT fetch API
   const updateToDo = (id, toDo) => {
     fetch(`http://localhost:8080/todos/${id}`, {
       method: 'PUT',
@@ -145,6 +176,7 @@
     .catch(error => console.error(error));
   }
   
+  //DELETE fetch API
   const deleteToDo = (id) => {
     fetch(`http://localhost:8080/todos/${id}`, {
       method: 'DELETE',
@@ -163,9 +195,4 @@
   }
   
   crudSelect.addEventListener("change", getOptionValue);
-  // getAllToDos();
-  // getOneToDo(1);
-  // deleteToDo(1);
-  // updateToDo(1, new ToDo("NO BANANA", true));
-  // addToDo(new ToDo("Buy shoes", false));
 })();
