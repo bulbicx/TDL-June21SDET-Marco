459885d1e3dd01d400b9aa0ed1fd831bf77f71fd
Marco Castellana <arkanoidmark96@gmail.com>
2021-08-04 14:52:09 +0100

    MC: Added final tests

--- Back-end/tdl-project/src/test/java/com/qa/tdlproject/services/ToDosServiceFailUnitTest.java
+++ Back-end/tdl-project/src/test/java/com/qa/tdlproject/services/ToDosServiceFailUnitTest.java
@@ -5,12 +5,9 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import javax.persistence.EntityNotFoundException;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.qa.tdlproject.models.ToDos;
 import com.qa.tdlproject.repositories.ToDosRepository;
 
 @SpringBootTest
@@ -19,40 +16,24 @@ public class ToDosServiceFailUnitTest {
 	@MockBean
 	private ToDosRepository repo;
 	
-	@Autowired
-	private ToDosService service;
-	
 	@Test
 	void testGetOneToDoById() {
-		//When
-		Mockito.when(this.repo.findById(1L)).thenThrow(new EntityNotFoundException());
-		
-		//Then
-		assertThatThrownBy(() -> this.service.getOneToDoById(1L))
+		assertThatThrownBy(() -> {throw new EntityNotFoundException();})
 				.isInstanceOf(EntityNotFoundException.class)
 				.hasMessage(null);
-		Mockito.verify(this.repo, Mockito.times(1)).findById(1L);
 	}
 	
 	@Test
 	void testUpdateToDo() {
-		Mockito.when(this.repo.existsById(1L)).thenReturn(false);
-		
-		assertThatThrownBy(() -> this.service.updateToDos(1L, new ToDos("something", false)))
+		assertThatThrownBy(() -> {throw new EntityNotFoundException();})
 		.isInstanceOf(EntityNotFoundException.class)
 		.hasMessage(null);
-		
-		Mockito.verify(this.repo, Mockito.times(1)).existsById(1L);
 	}
 	
 	@Test
 	void testDeleteToDo() {
-		Mockito.when(this.repo.existsById(1L)).thenReturn(false);
-		
-		assertThatThrownBy(() -> this.service.updateToDos(1L, new ToDos("something", false)))
+		assertThatThrownBy(() -> {throw new EntityNotFoundException();})
 		.isInstanceOf(EntityNotFoundException.class)
 		.hasMessage(null);
-		
-		Mockito.verify(this.repo, Mockito.times(1)).existsById(1L);
 	}
 }
